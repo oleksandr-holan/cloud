fc280b4e47a065e3c0b77b81cc3d0ac00a844567
Oleksandr Holan <game.play.for.alex@gmail.com>
2025-04-26 20:58:34 +0300

    add destroy provisioner

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -91,35 +91,21 @@ resource "aws_instance" "lab_instance" {
     Name = "Lab6-EC2-Instance"
   })
 
-  user_data = <<-EOT
-#!/bin/bash
-# Install necessary tools
-dnf install -y jq git libicu
+  user_data = local.user_data
 
-# Create a dedicated user for the GitHub runner
-useradd -m github-runner
+  provisioner "remote-exec" {
+    when = destroy
 
-# Create a folder
-mkdir -p /home/github-runner/actions-runner
-cd /home/github-runner/actions-runner
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = tls_private_key.generated_ssh_key.private_key_pem
+      host        = self.public_ip
+    }
 
-# Download the latest runner package
-curl -o actions-runner-linux-x64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz
-
-# Optional: Validate the hash
-echo "0dbc9bf5a58620fc52cb6cc0448abcca964a8d74b5f39773b7afcad9ab691e19  actions-runner-linux-x64-2.323.0.tar.gz" |  sha256sum -c
-# Extract the installer
-tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz
-# Set ownership
-chown -R  github-runner:github-runner /home/github-runner/actions-runner
-
-
-REG_TOKEN=$(curl -sX POST -H "Authorization: Bearer ${var.github_token}" https://api.${local.github.host}/repos/${local.github.owner}/${local.github.repo}/actions/runners/registration-token | jq .token --raw-output)
-sudo -u github-runner ./config.sh \
-  --unattended \
-  --url "${var.github_repo}" \
-  --token "$REG_TOKEN" \
-sudo ./svc.sh install github-runner
-sudo ./svc.sh start
-EOT
+    inline = [
+      "cd /home/github-runner/actions-runner",
+      "sudo -u github-runner ./config.sh remove --token $(curl -s -X POST -H \"Authorization: Bearer ${var.github_token}\" https://api.github.com/repos/${local.github.owner}/${local.github.repo}/actions/runners/remove-token | jq -r .token)"
+    ]
+  }
 }
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,3 +9,37 @@ locals {
     repo  = element(local._path_parts, 2)
   }
 }
+
+locals {
+  user_data = <<-EOT
+#!/bin/bash
+# Install necessary tools
+dnf install -y jq git libicu
+
+# Create a dedicated user for the GitHub runner
+useradd -m github-runner
+
+# Create a folder
+mkdir -p /home/github-runner/actions-runner
+cd /home/github-runner/actions-runner
+
+# Download the latest runner package
+curl -o actions-runner-linux-x64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz
+
+# Optional: Validate the hash
+echo "0dbc9bf5a58620fc52cb6cc0448abcca964a8d74b5f39773b7afcad9ab691e19  actions-runner-linux-x64-2.323.0.tar.gz" |  sha256sum -c
+# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz
+# Set ownership
+chown -R  github-runner:github-runner /home/github-runner/actions-runner
+
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: Bearer ${var.github_token}" https://api.${local.github.host}/repos/${local.github.owner}/${local.github.repo}/actions/runners/registration-token | jq .token --raw-output)
+sudo -u github-runner ./config.sh \
+  --unattended \
+  --url "${var.github_repo}" \
+  --token "$REG_TOKEN" \
+sudo ./svc.sh install github-runner
+sudo ./svc.sh start
+EOT
+}
