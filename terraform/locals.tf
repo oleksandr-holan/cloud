4e50a2e2e26393d5d9e36a4cde7cdae280e2213c
Oleksandr Holan <game.play.for.alex@gmail.com>
2025-04-26 23:23:45 +0300

    fix: set up github runner after docker setup

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -20,6 +20,28 @@ sudo dnf update
 # Install necessary tools
 dnf install -y jq git libicu docker
 
+# --- Set up Docker ---
+
+# Start the Docker service
+systemctl start docker
+systemctl enable docker
+
+# Add the ec2-user and github-runner users to the docker group to run Docker commands without using sudo.
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker github-runner
+
+# Create the directory for Docker CLI plugins
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+
+# Download the latest Docker Compose plugin for your architecture
+sudo curl -sL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
+
+# Set ownership and permissions
+sudo chown root:root /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+# --- Set up GitHub runner ---
+
 # Create a dedicated user for the GitHub runner
 useradd -m github-runner
 
@@ -44,23 +66,5 @@ sudo -u github-runner ./config.sh \
   --token "$REG_TOKEN" \
 ./svc.sh install github-runner
 ./svc.sh start
-
-# Start the Docker service
-systemctl start docker
-systemctl enable docker
-
-# Add the ec2-user and github-runner users to the docker group to run Docker commands without using sudo.
-sudo usermod -a -G docker ec2-user
-sudo usermod -a -G docker github-runner
-
-# Create the directory for Docker CLI plugins
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-
-# Download the latest Docker Compose plugin for your architecture
-sudo curl -sL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
-
-# Set ownership and permissions
-sudo chown root:root /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 EOT
 }
